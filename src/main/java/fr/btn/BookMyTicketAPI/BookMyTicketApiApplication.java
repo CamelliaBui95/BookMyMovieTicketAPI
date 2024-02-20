893c14b8cc8ae5c048638b94101f457f86ad84bc
TuongNghiBui <camellia.bui95@gmail.com>
2024-02-20 14:37:17 +0100

    Setting up project

---
+++ src/main/java/fr/btn/BookMyTicketAPI/BookMyTicketApiApplication.java
@@ -0,0 +1,13 @@
+package fr.btn.BookMyTicketAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyTicketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyTicketApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/btn/BookMyTicketAPI/BookMyTicketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.btn.BookMyTicketAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyTicketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
